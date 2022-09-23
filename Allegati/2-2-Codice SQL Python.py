1eedbd2fa699f632a5505d3057f29eadae5042d5
manuelcaccone <62841081+manuelcaccone@users.noreply.github.com>
2022-09-23 16:44:31 +0200

    Create 2-2-Codice SQL Python.py
    update

---
+++ Allegati/2-2-Codice SQL Python.py
@@ -0,0 +1,26 @@
+# apro la connessione al database, provo fetching con tutte le righe
+import pymysql
+from tabulate import tabulate
+import pandas as pd
+
+# Open database connection
+db = pymysql.connect(host='db4free.net',
+        user='prova_admin', 
+        password = "prova_admin",
+        db='provadbsia')
+        
+try:
+
+    with db.cursor() as cur:
+
+        cur.execute('SELECT * FROM claims')
+
+        data = cur.fetchall()
+finally:
+
+    db.close()
+
+num_fields = len(cur.description)
+field_names = [i[0] for i in cur.description]
+df = pd.DataFrame.from_records(data,columns=field_names)  
+print(tabulate(df.head(),headers=field_names))  
