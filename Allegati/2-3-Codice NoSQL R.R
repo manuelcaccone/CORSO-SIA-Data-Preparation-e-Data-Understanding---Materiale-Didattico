669bbc468b7f6276d3f71f248c1498f06aca3efc
manuelcaccone <62841081+manuelcaccone@users.noreply.github.com>
2022-10-03 16:29:35 +0200

    commit

---
+++ Allegati/2-3-Codice NoSQL R.R
@@ -0,0 +1,31 @@
+# importo i dati dal Db 
+res=DBI::dbSendQuery(con,'SELECT * FROM claims;')
+res=dbFetch(res)
+dbClearResult(dbListResults(con)[[1]])
+claims=res
+
+# calcolo importo medio del liquidato per peril
+
+library(dplyr)
+claims %>% 
+  group_by(peril=Descrizione) %>%
+  summarize(Mean_Cost=mean(ImportoLiquidato))
+
+# calcolo numero di sinistri per peril
+
+claims %>% 
+  group_by(peril=Descrizione) %>%
+  summarize(Num_Sx=length(ID))
+
+# calcolo numero di sinistri per peril con data.table
+
+library(data.table)
+claims=data.table(claims)
+claims[,.(Mean_Cost=mean(ImportoLiquidato)),by=.(Peril=Descrizione)]
+
+# calcolo il premio equo per peril 
+
+claims[,D:=.N]
+claims[,.(Equity_Premium=mean(ImportoLiquidato)*length(Descrizione)/unique(D)),by=.(Peril=Descrizione)]
+
+
